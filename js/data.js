e254bf29e05e2516578a1af4552f39812334ce47
Steven Pescheteau <steven.pescheteau@gmail.com>
2016-12-30 08:47:51 +0100

    Rules added in index and data

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-var LIMIT_POINTS = 30;
+var LIMIT_POINTS = 14;
 
 var classes = [
   new Classe('Crâ',        0)
@@ -18,6 +18,29 @@ var classes = [
 , new Classe('Steamer',    1)
 , new Classe('Xélor',      4)
 , new Classe('Zobal',      1)
+];
+
+var rules = [
+  new Rule(classes[1],  classes[4],  1) // Enu   Eca   1
+, new Rule(classes[2],  classes[4],  1) // Enu   Elio  1
+, new Rule(classes[4],  classes[7],  1) // Enu   Iop   1
+, new Rule(classes[4],  classes[11], 1) // Enu   Sacri 1
+, new Rule(classes[4],  classes[15], 1) // Enu   Xel   1
+, new Rule(classes[1],  classes[2],  1) // Eca   Elio  1
+, new Rule(classes[2],  classes[9],  1) // Dada  Elio  1
+, new Rule(classes[1],  classes[12], 1) // Sadi  Eca   1
+, new Rule(classes[5],  classes[12], 1) // Sadi  Feca  1
+, new Rule(classes[8],  classes[12], 1) // Sadi  Osa   1
+, new Rule(classes[12], classes[13], 1) // Sadi  Sram  1
+, new Rule(classes[12], classes[16], 1) // Sadi  Zob   1
+, new Rule(classes[14], classes[16], 1) // Stea  Zob   1
+
+, new Rule(classes[3],  classes[8],  2) // Eni   Osa   2
+, new Rule(classes[3],  classes[11], 2) // Eni   Sacr  2
+, new Rule(classes[3],  classes[15], 2) // Eni   Xel   2
+, new Rule(classes[8],  classes[11], 2) // Osa   Sacr  2
+, new Rule(classes[11], classes[15], 2) // Xel   Sacr  2
+, new Rule(classes[8],  classes[15], 2) // Xel   Osa   2
 ];
 
 var teams = [
